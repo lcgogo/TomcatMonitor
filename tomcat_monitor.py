c88423433add9c25d50db7afc806e82cbf40f524
root <root@kube-node1>
2018-03-13 12:30:26 +0000

    set tomcat as a constant PROCESS

--- tomcat_monitor.py
+++ tomcat_monitor.py
@@ -14,15 +14,16 @@ RESTART_WAIT = 5 # Seconds. Time wait after restart process.
 LOG_FILE = "/var/log/tomcat/catalina.2018-03-08.log"
 #ERROR_KEYWORD = "OutOfMemory"
 ERROR_KEYWORD = "error"
+PROCESS = "tomcat"
 
 # def return code
 # 0 is normal, others are error.
 
 def StatusCheck():
-    tomcatStatus = os.system("systemctl status tomcat")
-    #print(tomcatStatus)
-    tomcatStatus = tomcatStatus >> 8 #get the 'echo #?' of 'systemctl status tomcat'
-    if tomcatStatus != 0:
+    processStatus = os.system("systemctl status %s" % PROCESS)
+    #print(processStatus)
+    processStatus = processStatus >> 8 #get the 'echo #?' of 'systemctl status $PROCESS'
+    if processStatus != 0:
         return 1
     else:
         return 0
@@ -49,22 +50,22 @@ def RequestTest():
         return 0
 
 def RestartTomcat():
-    os.system("systemctl restart tomcat")
-    print("Tomcat restarted.")
+    os.system("systemctl restart %s" % PROCESS)
+    print("%s restarted." % PROCESS)
     time.sleep(RESTART_WAIT)
     if StatusCheck() != 0:
-        print("Tomcat restart is failed, need manual check with 'systemctl status tomcat'.")
+        print("%s restart is failed, need manual check with 'systemctl status %s'." % PROCESS)
         return 1
     else:
-        print("Tomcat restart is OK.")
+        print("%s restart is OK." % PROCESS)
         return 0 
 
 def main():
     resultStatusCheck = StatusCheck()
-    if resultStatusCheck != 0: print("Tomcat status is inactive.")
+    if resultStatusCheck != 0: print("%s status is inactive." % PROCESS)
 
     resultLogCheck = LogCheck()
-    if resultLogCheck != 0: print('Find error "%s" in tomcat log "%s".' % (ERROR_KEYWORD, LOG_FILE))
+    if resultLogCheck != 0: print('Find error "%s" in log "%s".' % (ERROR_KEYWORD, LOG_FILE))
 
     if resultStatusCheck == 0 and resultLogCheck == 0:
         resultRequestTest = RequestTest()
@@ -73,7 +74,7 @@ def main():
         resultRequestTest = 0
 
     if resultStatusCheck != 0 or resultLogCheck != 0 or resultRequestTest != 0:        
-        print("Need restart tomcat.")
+        print("Need restart %s." % PROCESS)
         RestartTomcat()
 
 if __name__ == "__main__": main()
